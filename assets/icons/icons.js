f6fa801ad0132b7e85ea88d3d65ec4997f327902
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-10-03 21:27:31 +0300

    192627 / Верстка GUI: базовые блоки - сверстаны базовые блоки указанные в задании

--- assets/icons/icons.js
+++ assets/icons/icons.js
@@ -1,2 +1,20 @@
 // Пример подключения иконки
 // import './svg/my-icon.svg';
+
+import './svg/next.svg';
+import './svg/loupe.svg';
+import './svg/cursor-right.svg';
+import './svg/logo.svg';
+import './svg/logo-small.svg';
+import './svg/instagram.svg';
+import './svg/youtube.svg';
+import './svg/star.svg';
+import './svg/heart.svg';
+import './svg/comment.svg';
+import './svg/eye.svg';
+import './svg/important.svg';
+import './svg/question.svg';
+import './svg/burger-menu.svg';
+import './svg/profile.svg';
+import './svg/close.svg';
+import './svg/apply.svg';
